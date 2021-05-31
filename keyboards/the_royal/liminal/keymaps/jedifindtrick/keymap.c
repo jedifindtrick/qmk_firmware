18be7849ed79d494d4e8466d938eec3dc79b6f04
Alexander <48211557+jedifindtrick@users.noreply.github.com>
2021-05-31 09:09:08 +0200

    Update keymap.c

--- keyboards/the_royal/liminal/keymaps/jedifindtrick/keymap.c
+++ keyboards/the_royal/liminal/keymaps/jedifindtrick/keymap.c
@@ -122,10 +122,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * └─────┴────┴────┴─────┴────────────┴──────────┴───────┴──────┴─────┴──────┘
    */
   [_OTHER] = LAYOUT_base_kit_all(
-    KC_ESC,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_UP,      _______,     _______,
-    KC_2,    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_LEFT,   KC_DOWN,    KC_RIGHT,    KC_ENTER,
-    KC_3,    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,     _______,     _______,
-    KC_4,    KC_LCTL, KC_LGUI, KC_LALT,           KC_SPACE,        KC_SPACE,         TD(TD_RST),         _______,    _______,     _______
+    KC_ESC,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_UP,      _______,    _______,    _______,
+    KC_2,    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_LEFT,   KC_DOWN,    KC_RIGHT,   _______,    KC_ENTER,
+    KC_3,    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,      KC_COMM,    KC_DOT,     _______,    _______,
+    KC_4,    KC_LCTL, KC_LGUI, KC_LALT,           KC_SPACE,        KC_SPACE,         TD(TD_RST),            _______,    _______,    _______
   )
 
 };
